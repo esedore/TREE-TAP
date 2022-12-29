ba785dd03350fbe0bd232260f4715abc984d81ed
eric <ericsedore88@gmail.com>
2022-12-29 15:36:53 -0500

    DPI

--- DPI_Gauss.py
+++ DPI_Gauss.py
@@ -1,7 +1,6 @@
 import sys
 import wmi
-import numpy as np
-import socket
+from scapy.all import *
 import struct
 from scipy.stats import norm
 
@@ -28,34 +27,20 @@ selected_adapter = adapters[selected_adapter - 1]
 # Get the selected adapter's MAC address
 mac_address = selected_adapter.MACAddress
 
-# Create a raw socket using the AF_PACKET socket type
-sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(0x0003))
+#Live capture packet function
+def packet_callback(packet):
 
-# Bind the socket to the selected network adapter's MAC address
-sock.bind((selected_adapter.Description, 0))
-
-# Set the socket to promiscuous mode
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2**30)
-sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-
-# Capture packets from the network
-while True:
-    # Receive a packet from the network
-    packet = sock.recv(65565)
-    
-    # Unpack the packet header
-    header = struct.unpack("!6s6sH", packet[:14])
-    source_mac = header[0]
-    destination_mac = header[1]
-    ethertype = header[2]
-    
     # Get the packet size
     packet_size = len(packet)
-    
-    # Fit a Gaussian distribution to the packet size data
+
+    # Print the packet size
+    print(packet_size)
+
+    #Gaussian Anomaly Detection of packet size
     mu, std = norm.fit(packet_size)
     if packet_size > mu + 3*std:
       print("Anomaly detected: packet size ="), packet_size
       sys.stdout = log_file
       log_file.close()
-  
+      
+sniff(iface=selected_adapter.Description, prn=packet_callback)
