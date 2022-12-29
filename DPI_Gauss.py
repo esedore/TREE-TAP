2a6e20ef051284ad1668cd1b2040ecaabfc84820
eric <ericsedore88@gmail.com>
2022-12-29 15:56:41 -0500

    Update DPI_Gauss.py

--- DPI_Gauss.py
+++ DPI_Gauss.py
@@ -5,42 +5,40 @@ import struct
 from scipy.stats import norm
 
 #Set logging directory
-log_file = open(r"logs\anomaly.log", "w")
+with open(r"logs\anomaly.log", "a") as log_file:
+  # Create a WMI object
+  c = wmi.WMI()
 
-# Create a WMI object
-c = wmi.WMI()
+  # Get a list of all network adapters
+  adapters = c.Win32_NetworkAdapterConfiguration(IPEnabled=True)
 
-# Get a list of all network adapters
-adapters = c.Win32_NetworkAdapterConfiguration(IPEnabled=True)
+  # Print a list of the network adapters
+  print("Select a network adapter:")
+  for i, adapter in enumerate(adapters):
+      print(f"{i+1}. {adapter.Description} ({adapter.IPAddress[0]})")
 
-# Print a list of the network adapters
-print("Select a network adapter:")
-for i, adapter in enumerate(adapters):
-    print(f"{i+1}. {adapter.Description} ({adapter.IPAddress[0]})")
+  # Prompt the user to select a network adapter
+  selected_adapter = int(input("Enter the number of the adapter you want to use: "))
 
-# Prompt the user to select a network adapter
-selected_adapter = int(input("Enter the number of the adapter you want to use: "))
+  # Get the selected network adapter
+  selected_adapter = adapters[selected_adapter - 1]
 
-# Get the selected network adapter
-selected_adapter = adapters[selected_adapter - 1]
+  # Get the selected adapter's MAC address
+  mac_address = selected_adapter.MACAddress
 
-# Get the selected adapter's MAC address
-mac_address = selected_adapter.MACAddress
+  #Live capture packet function
+  def packet_callback(packet):
 
-#Live capture packet function
-def packet_callback(packet):
+      # Get the packet size
+      packet_size = len(packet)
 
-    # Get the packet size
-    packet_size = len(packet)
+      # Print the packet size
+      print(packet_size)
 
-    # Print the packet size
-    print(packet_size)
+      #Gaussian Anomaly Detection of packet size
+      mu, std = norm.fit(packet_size)
+      if packet_size > mu + 3*std:
+        print("Anomaly detected: packet size ="), packet_size
+        sys.stdout = log_file
 
-    #Gaussian Anomaly Detection of packet size
-    mu, std = norm.fit(packet_size)
-    if packet_size > mu + 3*std:
-      print("Anomaly detected: packet size ="), packet_size
-      sys.stdout = log_file
-      log_file.close()
-      
-sniff(iface=selected_adapter.Description, prn=packet_callback)
+  sniff(iface=selected_adapter.Description, prn=packet_callback)
